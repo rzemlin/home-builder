69c7f3235dac55cd4da166844ebe8718d3077274
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-07 08:48:18 -0800

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,27 +1,22 @@
 class ApplicationController < ActionController::Base
-before_action :current_user
+    include ApplicationHelper::SessionsHelper
+    before_action :current_user
+    
     def home
     
     end
 
-   
-   #helpers
-   private
-    def current_user
-        if session[:current_user_id]
-            @current_user = User.find(session[:current_user_id])
-        end
-    end
-
-    def login(user)
-        session[:current_user_id] = @user.id
-    end
-
-
-
-
     def logged_in?
         session.include? :user_id
       end
+
+    private
+
+    def authentication_required
+        if !logged_in?
+            redirect_to "/login"
+        end
+    end
+    
 
 end
