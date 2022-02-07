f30ccd584e966ad7450d97818a0e6a0cbe06038c
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-06 20:59:56 -0800

    add helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,19 @@ before_action :current_user
     
     end
 
+   
+   #helpers
+   private
     def current_user
         if session[:current_user_id]
             @current_user = User.find(session[:current_user_id])
         end
     end
+
+    def login(user)
+        session[:current_user_id] = @user.id
+    end
+
 
 
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,12 +7,17 @@ class SessionsController < ApplicationController
     def create
         @user = User.find_by(:email => params[:email])
         if @user
-            session[:current_user_id] = @user.id
+            login(@user)
             redirect_to "/"
         else
             redirect_to "/login", :notice => "couldn't find the user"
         end
+    
+    end
 
+    def destroy
+        reset_session
+        redirect_to '/'
 
     end
 
