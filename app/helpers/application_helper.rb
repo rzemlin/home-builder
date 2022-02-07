162fe8e053fbfc2b2bc983ab668d7d8ec5f0a9c0
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-07 08:27:55 -0800

    add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,18 @@
 module ApplicationHelper
     module SessionsHelper
+        def logged_in? #three different ways to check if logged in
+         #   if current_user
+         #       true
+         #   else
+         #       false
+         #   end
+         #   current_user ? true : false
+         !!current_user
+        end
+        
         def current_user
             if session[:current_user_id]
-                @current_user = User.find(session[:current_user_id])
+                @current_user ||= User.find(session[:current_user_id])
             end
         end
     
