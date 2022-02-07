e3e42dd97d419c6474f05620872fd40c1cb21e52
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-06 20:59:28 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema.define(version: 2022_02_06_182941) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
