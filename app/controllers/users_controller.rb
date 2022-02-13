dd0de8aae35e0e18eb932e5bdbe5a0d34d04c110
rzemlin <34727847+rzemlin@users.noreply.github.com>
2022-02-13 12:10:36 -0800

    fix db

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
+    #GET request for new user action
     def new 
         
     end
     
+    #POST request to create a new user
     def create
-    
+    @user = User.new
+    @user.name = params[:name]
+    @user.email = params[:email]
+    @user.password_digest = params[:password]
+    @user.save
+    redirect_to '/login'
     end
 
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_13_155937) do
-
-  create_table "houses_tables", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.text "address"
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "materials", force: :cascade do |t|
-    t.string "name"
-    t.integer "quantity"
-    t.text "description"
-  end
+ActiveRecord::Schema.define(version: 2022_02_13_195000) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
